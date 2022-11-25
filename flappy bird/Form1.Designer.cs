ad547feaeef54f493aa265dafb67f5c77a2c4f87
michal vetiška <michalvetiska@email.cz>
2022-11-25 14:07:03 +0100

    ver2

--- flappy bird/Form1.Designer.cs
+++ flappy bird/Form1.Designer.cs
@@ -30,70 +30,62 @@
         {
             this.components = new System.ComponentModel.Container();
             this.ptacek = new System.Windows.Forms.PictureBox();
-            this.groundimg = new System.Windows.Forms.PictureBox();
+            this.zem = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.pipeDown = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pipeUP = new System.Windows.Forms.PictureBox();
             this.labelskore = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.ptacek)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.groundimg)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zem)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pipeDown)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pipeUP)).BeginInit();
             this.SuspendLayout();
             // 
             // ptacek
             // 
             this.ptacek.Image = global::flappy_bird.Properties.Resources.Flappy_Bird2;
-            this.ptacek.Location = new System.Drawing.Point(61, 141);
+            this.ptacek.Location = new System.Drawing.Point(87, 235);
+            this.ptacek.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.ptacek.Name = "ptacek";
-            this.ptacek.Size = new System.Drawing.Size(85, 75);
+            this.ptacek.Size = new System.Drawing.Size(121, 125);
             this.ptacek.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.ptacek.TabIndex = 0;
             this.ptacek.TabStop = false;
             // 
-            // groundimg
+            // zem
             // 
-            this.groundimg.Image = global::flappy_bird.Properties.Resources.R;
-            this.groundimg.Location = new System.Drawing.Point(-27, 497);
-            this.groundimg.Name = "groundimg";
-            this.groundimg.Size = new System.Drawing.Size(1033, 67);
-            this.groundimg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.groundimg.TabIndex = 1;
-            this.groundimg.TabStop = false;
+            this.zem.Image = global::flappy_bird.Properties.Resources.R;
+            this.zem.Location = new System.Drawing.Point(-39, 828);
+            this.zem.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.zem.Name = "zem";
+            this.zem.Size = new System.Drawing.Size(1476, 133);
+            this.zem.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.zem.TabIndex = 1;
+            this.zem.TabStop = false;
             // 
             // timer1
             // 
-            this.timer1.Interval = 200;
+            this.timer1.Interval = 41;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // pipeDown
             // 
             this.pipeDown.Image = global::flappy_bird.Properties.Resources.pipe_down;
-            this.pipeDown.Location = new System.Drawing.Point(735, 1);
+            this.pipeDown.Location = new System.Drawing.Point(1050, 2);
+            this.pipeDown.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pipeDown.Name = "pipeDown";
-            this.pipeDown.Size = new System.Drawing.Size(100, 100);
+            this.pipeDown.Size = new System.Drawing.Size(143, 167);
             this.pipeDown.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pipeDown.TabIndex = 3;
             this.pipeDown.TabStop = false;
             // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = global::flappy_bird.Properties.Resources.WEATHER;
-            this.pictureBox2.Location = new System.Drawing.Point(-1, 1);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(99, 83);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 4;
-            this.pictureBox2.TabStop = false;
-            // 
             // pipeUP
             // 
             this.pipeUP.Image = global::flappy_bird.Properties.Resources.pipe_up;
-            this.pipeUP.Location = new System.Drawing.Point(467, 318);
+            this.pipeUP.Location = new System.Drawing.Point(667, 530);
+            this.pipeUP.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pipeUP.Name = "pipeUP";
-            this.pipeUP.Size = new System.Drawing.Size(100, 180);
+            this.pipeUP.Size = new System.Drawing.Size(143, 300);
             this.pipeUP.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pipeUP.TabIndex = 5;
             this.pipeUP.TabStop = false;
@@ -102,33 +94,33 @@
             // 
             this.labelskore.AutoSize = true;
             this.labelskore.BackColor = System.Drawing.Color.DarkKhaki;
-            this.labelskore.Location = new System.Drawing.Point(98, 524);
+            this.labelskore.Location = new System.Drawing.Point(140, 873);
+            this.labelskore.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelskore.Name = "labelskore";
-            this.labelskore.Size = new System.Drawing.Size(48, 15);
+            this.labelskore.Size = new System.Drawing.Size(76, 25);
             this.labelskore.TabIndex = 6;
             this.labelskore.Text = "Skóre: 0";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSkyBlue;
-            this.ClientSize = new System.Drawing.Size(971, 559);
+            this.ClientSize = new System.Drawing.Size(1406, 935);
             this.Controls.Add(this.labelskore);
             this.Controls.Add(this.pipeUP);
-            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pipeDown);
-            this.Controls.Add(this.groundimg);
+            this.Controls.Add(this.zem);
             this.Controls.Add(this.ptacek);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Flappy bird ";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             ((System.ComponentModel.ISupportInitialize)(this.ptacek)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.groundimg)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zem)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pipeDown)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pipeUP)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -138,11 +130,10 @@
         #endregion
 
         private PictureBox ptacek;
-        private PictureBox groundimg;
+        private PictureBox zem;
         private System.Windows.Forms.Timer timer1;
         //private Label skore;
         private PictureBox pipeDown;
-        private PictureBox pictureBox2;
         private PictureBox pipeUP;
         private Label labelskore;
     }
--- flappy bird/Form1.cs
+++ flappy bird/Form1.cs
@@ -7,20 +7,20 @@ namespace flappy_bird
             InitializeComponent();
         }
         int gravitace = 10;
-        int rychlost = 25;
+        int rychlost = 10;
         int skore = 0;
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
             if(e.KeyCode == Keys.Space)
             { 
-            gravitace= 15;
+            gravitace= 20;
             }
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Space)
-            gravitace = -15;
+            gravitace = -20;
             
             else if (e.KeyCode == Keys.Enter)
                 timer1.Start();
@@ -28,10 +28,38 @@ namespace flappy_bird
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            Random rnd = new Random();
             ptacek.Top += gravitace;
             pipeDown.Left -= rychlost;
             pipeUP.Left -= rychlost;
             labelskore.Text = "Skóre: " + skore;
+            //poèítání skóre
+            if (pipeDown.Left < -185)
+            { 
+                pipeDown.Left = rnd.Next(100, 600);
+                skore++;
+            }
+            if(pipeUP.Left <- 185)
+            {
+                pipeUP.Left = rnd.Next(300, 850);
+                skore++;
+            }
+            if(ptacek.Bounds.IntersectsWith(pipeUP.Bounds) || ptacek.Bounds.IntersectsWith(pipeDown.Bounds) || ptacek.Bounds.IntersectsWith(zem.Bounds))
+            {
+                timer1.Stop();
+                MessageBox.Show("Konec hry!", "Prohrál/a jste",  MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            rychlost = 10 + skore;
+            
+                /*
+                if(rychlost >30)
+                    rychlost += 1;
+                else if(rychlost > 50)
+                    rychlost += 0,2;
+                else if (rychlost > 100)
+                    rychlost += 0,05;*/
+            
+
         }
     }
 }
